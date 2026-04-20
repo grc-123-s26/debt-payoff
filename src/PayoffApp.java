7e6e26687a6a6df35e8d85f15529714d3a558ab6
Necobi <274595030+Necobi@users.noreply.github.com>
2026-04-20 11:04:59 -0700

    Added arraylist to properly order the APR's

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,13 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class PayoffApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        //interface on left, concrete structure on right
+        List<Double> aprs = new ArrayList<>();
+
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
 
             double apr = scan.nextDouble();
+
+            aprs.add(apr);
+
+
             double balance = scan.nextDouble();
 
             // Consume \n after balance input 
@@ -17,5 +28,10 @@ public class PayoffApp {
             String balanceString = String.format("$%.2f", balance);
             System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
         }
+
+        //reverseOrder() makes it so the order is reversed from default (acse -> desc )
+        Collections.sort(aprs, Collections.reverseOrder());
+
+        System.out.println(aprs);
     }
 }
