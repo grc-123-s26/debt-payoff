5675ae2d294f70d4bd9a0c1fc314287f320501d5
kohumukini <kealii.ohumukini@gmail.com>
2026-04-20 11:03:32 -0700

    Adjusted Payoff and CreditCard

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,15 @@
-public class CreditCard {
-    
+public class CreditCard implements Comparable<CreditCard> {
+    String name = ""; 
+    double apr; 
+    double balance;
+
+    public CreditCard(String name, double apr, double balance) {
+        this.name = name; 
+        this.apr = apr; 
+        this.balance = balance; 
+    }
+
+    public int compareTo(CreditCard other) {
+        return 0; 
+    }
 }
--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,14 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
+// alt + shift + o
+
 public class PayoffApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner scan = new Scanner(System.in);
+
+        List<Double> aprs = new ArrayList<>(); 
 
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
 
             double apr = scan.nextDouble();
             double balance = scan.nextDouble();
+
+            aprs.add(apr); 
 
             // Consume \n after balance input 
             if(scan.hasNextLine()) scan.nextLine();
@@ -17,5 +26,8 @@ public class PayoffApp {
             String balanceString = String.format("$%.2f", balance);
             System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
         }
+        Collections.sort(aprs, Collections.reverseOrder());
+
+        System.out.println(aprs); 
     }
 }
