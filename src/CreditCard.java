a0130c48a4b309d38774d1b98fdb03851b2fc1b9
Necobi <274595030+Necobi@users.noreply.github.com>
2026-04-20 11:42:33 -0700

    Implemented Credit Card class

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,23 @@
 public class CreditCard {
+    private String name;
+    private double apr;
+    private double balance;
+
+    public CreditCard(String name, double apr, double balance){
+        //this is talking about "this credit card that is being made right now" if you're using the same variable name as the instance variable
+        this.name = name;
+        this.apr = apr;
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCard Name: " + name + " APR: " + apr + ", Balance: " + balance ;
+    }
+
+    public String getName(){
+        //since it is a private instance you need to make a get name so you can have access to it in teh payoff class 
+        return this.name;
+    }
     
 }
--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -8,8 +8,12 @@ public class PayoffApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        CreditCard amex = new CreditCard("American Express", 18.40, 400);
+        System.out.println(amex);
+
         //interface on left, concrete structure on right
         List<Double> aprs = new ArrayList<>();
+        List<CreditCard> cards = new ArrayList<>();
 
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
@@ -20,6 +24,8 @@ public class PayoffApp {
 
 
             double balance = scan.nextDouble();
+            
+            cards.add(new CreditCard(name, apr, balance));
 
             // Consume \n after balance input 
             if(scan.hasNextLine()) scan.nextLine();
@@ -31,7 +37,9 @@ public class PayoffApp {
 
         //reverseOrder() makes it so the order is reversed from default (acse -> desc )
         Collections.sort(aprs, Collections.reverseOrder());
+       //Collections.sort(cards); use comparable interface 
 
         System.out.println(aprs);
+        System.out.println(cards);
     }
 }
