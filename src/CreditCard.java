86a08f8a1593e82d4bf4257b7a78ee4cb291bb97
tk421 <42457203+tk421bsod@users.noreply.github.com>
2026-04-20 11:41:15 -0700

    Add fields to CreditCard.java

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,30 @@
 public class CreditCard {
+
+    private double apr;
+    private double balance;
+    private String name;
+
+    public CreditCard(double apr, double balance, String name) {
+        this.apr = apr;
+        this.balance = balance;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCard [apr=" + this.apr + ", balance=" + this.balance + ", name=" + this.name + "]";
+    }
+
+    public double getApr() {
+        return this.apr;
+    }
     
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
 }
