5e52828136e0d2a1f8b30457dfeb89cc35572471
Anh Ngo <Ngo.Anh.2@student.greenriver.edu>
2026-04-20 11:42:00 -0700

    end of class

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -7,7 +7,12 @@ public class PayoffApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        CreditCard amex = new CreditCard(" American Express", 18.4, 402);
+
+        System.out.println(amex);
+
         List<Double> aprs = new ArrayList<>(); 
+        List<CreditCard> cards = new ArrayList<>(); 
         
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
@@ -18,6 +23,11 @@ public class PayoffApp {
 
             double balance = scan.nextDouble();
 
+            cards.add(new CreditCard(name,apr,balance));
+
+            //CreditCard myCard = new CreditCard(name, apr, balance);
+            //cards.add(myCard); 
+
             // Consume \n after balance input 
             if(scan.hasNextLine()) scan.nextLine();
 
@@ -27,7 +37,9 @@ public class PayoffApp {
         }
 
         Collections.sort(aprs,Collections.reverseOrder());
+        //Collections.sort(cards); 
 
         System.out.println(aprs);
+        System.out.println(cards);
     }
 }
