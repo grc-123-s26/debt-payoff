a040653465b514c294410b6bb03281e916bffd6c
Elijah_Rhodes <mastergamer3dr@gmail.com>
2026-04-20 11:04:24 -0700

    Method one - Sorting aprs within the main code

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,13 @@
-public class CreditCard {
-    
+public class CreditCard{ //implements Comparable<APR>{
+/* 
+    private String name;
+    private double APR;
+    private double bal;
+
+    public int compareTo()
+    {
+
+        return 0;
+    }
+    */
 }
