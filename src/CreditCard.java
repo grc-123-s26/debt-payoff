adffce5344decf6499f87ff92dd6f6561a52ef5c
Viktor Huchiev <274128020+viktorHuc@users.noreply.github.com>
2026-04-20 11:43:20 -0700

    Added CreditCard class and list for cards in main to save it

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,20 @@
-public class CreditCard {
-    
+public class CreditCard{
+    private String name;
+    private double apr;
+    private double balance; 
+
+    public CreditCard(String name, double apr, double balance){
+        this.name = name;
+        this.apr = apr;
+        this.balance = balance;
+    }
+
+    public String getName(){return name;}
+
+    @Override
+    public String toString(){
+        String aprString = String.format("%.2f%%", apr);
+        String balanceString = String.format("$%.2f", balance);
+        return name + ": " + "APR: " + aprString + " Balance: " + balanceString;
+    }
 }
--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -8,7 +7,11 @@ public class PayoffApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        CreditCard amex =  new CreditCard("American Express", 18.4, 402);
+        System.out.println(amex);
+
         List<Double> aprs = new ArrayList<>();
+        List<CreditCard> cards = new ArrayList<>();
 
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
@@ -20,11 +23,15 @@ public class PayoffApp {
             // Consume \n after balance input 
             if(scan.hasNextLine()) scan.nextLine();
 
-            String aprString = String.format("%.2f%%", apr);
-            String balanceString = String.format("$%.2f", balance);
-            System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
+            // String aprString = String.format("%.2f%%", apr);
+            // String balanceString = String.format("$%.2f", balance);
+            // System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
+
+            cards.add(new CreditCard(name, apr, balance));
+            System.out.println(cards);
         }
         Collections.sort(aprs, Collections.reverseOrder());
+        //Collections.sort(cards);
         System.out.println(aprs);
     }
 }
